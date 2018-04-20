4ae2951d9cc247544af5acef5a37c0096b6b514d
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-04-20 15:57:32 -0600

    updated prompts for exercises

--- exercises/2.js
+++ exercises/2.js
@@ -1,4 +1,4 @@
-/* Get the data at `https://fetch-data-e2f88.firebaseapp.com/name.json` and return the `name`") */
+/* Get the data at `https://fetch-data-e2f88.firebaseapp.com/name.json` and return the `name`) */
 const fetch = require("node-fetch");
 
 module.exports = function(){
