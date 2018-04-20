b1990688b55fea54de8f652a6356d050867662d8
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-04-20 16:04:47 -0600

    fixed indentation

--- tests/4.js
+++ tests/4.js
@@ -6,11 +6,11 @@ describe("Exercise 4", () => {
   it("returns object of 'minions'", done => {
     exercise().then(result => {
       assert.deepEqual(result, {
-    "righthandWoman": "Kim",
-    "leadInstructors": ["Brooks", "Chad", "Dan"],
-    "associateInstructors": ["Peter", "Matt", "Schultz"],
-    "residentInstructors": ["Casey", "Lizz", "The Mann"]
-  });
+        "righthandWoman": "Kim",
+        "leadInstructors": ["Brooks", "Chad", "Dan"],
+        "associateInstructors": ["Peter", "Matt", "Schultz"],
+        "residentInstructors": ["Casey", "Lizz", "The Mann"]
+      });
       done();
     }).catch(done);
   });
