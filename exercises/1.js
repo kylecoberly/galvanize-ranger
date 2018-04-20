58cd4c87591bcaffb6c89e4c84207946e23191d0
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-04-20 15:43:41 -0600

    cleaned up pesky semicolons

--- exercises/1.js
+++ exercises/1.js
@@ -1,5 +1,5 @@
 /* Get the data at `https://fetch-data-e2f88.firebaseapp.com/name.json` and return the `name`) */
-const fetch = require("node-fetch");
+const fetch = require("node-fetch")
 
 module.exports = function(){
   return fetch("https://fetch-data-e2f88.firebaseapp.com/name.json")
--- exercises/7.js
+++ exercises/7.js
@@ -1,5 +1,5 @@
 /* Get the data at `https://fetch-data-e2f88.firebaseapp.com/name.json` and return the second and third indexes of `associateInstructors`) */
-const fetch = require("node-fetch");
+const fetch = require("node-fetch")
 
 module.exports = function(){
   return fetch("https://fetch-data-e2f88.firebaseapp.com/name.json")
